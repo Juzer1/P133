6855380048651685ba285ebfb52ef9a0c8612057
Juzer1 <95688069+Juzer1@users.noreply.github.com>
2022-08-08 21:01:52 +0530

    Add files via upload
    noob

---
+++ main.js
@@ -0,0 +1,12 @@
+function button1()
+{
+    window.location = "index1.html";
+}
+function button2()
+{
+    window.location = "index2.html";
+}
+function button3()
+{
+    window.location = "index3.html";
+}
---
+++ main1.js
@@ -0,0 +1,46 @@
+img = "";
+status1 = "";
+objects = [];
+function preload(){
+    img = loadImage('catorcutorcetorcot.jpeg');
+}
+function setup() {
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded)
+    document.getElementById("status").innerHTML = "status :Detecting Objects"
+
+}
+function draw() {
+    ImageBitmapRenderingContexte(img,0,0,640,420);
+    
+    if(status1 !="")
+    {for (i=0; i <objects.length ; i++)
+        {
+           document.getElementById("status").innerHTML = "status : Object Detected"
+
+            fill("#FF0000");
+            percent = floor(Objects[i].confidence * 100 );
+            text(objects[i].label + "" + percent + "%",objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status1 = true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult (error,results) {
+    if (error) {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    objects = results;
+    }
+   
+}
